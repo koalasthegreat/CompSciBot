37a360b17833de8f96d00741ab6560b8c2f9e6bd
Andrew Stacey <koalasthegreat@gmail.com>
2024-09-26 20:05:34 -0230

    incomplete progress

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM python:3.10
 
 ADD compscibot/ /run/compscibot/
-COPY alembic.ini .env requirements.txt /run
+COPY alembic.ini .env requirements.txt start_bot.sh /run
 
 WORKDIR "/run"
 
 RUN pip install -r requirements.txt
+RUN chmod +x start_bot.sh
 
-CMD ["python3", "-m", "compscibot"]
+CMD ["/run/start_bot.sh"]
--- compscibot/db.py
+++ compscibot/db.py
@@ -1,9 +1,8 @@
-from compscibot.config import DB_URL
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
 
-engine = create_async_engine(f"sqlite+aiosqlite://{DB_URL}")
+engine = create_async_engine("sqlite+aiosqlite:///run/data/bot.db")
 
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
